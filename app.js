950a1f76195b49456be868453e13d7fa48214ec9
Shanthee Sampath <shantheesam@gmail.com>
2017-01-03 15:40:03 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+// function def for finding max of 3 numbers
+function find_max(num1,num2,num3) {
+	var maximum = Math.max(num1,num2,num3);
+	return maximum;
+}
+
+//to print on the console
+var maximum = find_max(10,5,6);
+console.log(maximum);
+
